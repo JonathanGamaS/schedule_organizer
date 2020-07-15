b4d29c949d8cac4deb307c39ac8ee4e8a49ea963
JonathanGamaS <jonathan_gama_@hotmail.com>
2020-07-14 22:30:25 -0300

    UPDATE: Versao 1.0 da aplicação que retorna o programa sendo transmitido no mesmo horario da requisição

--- orchestrator_programming.py
+++ orchestrator_programming.py
@@ -2,12 +2,18 @@ import requests
 import json
 import calendar
 import time
+from datetime import datetime
+import pytz
+
 
 def execute_orchestrator_programming():
+    programacao_atual = "Init"
     programacao_total = globo_get_endpoint()
     horario = get_time_now()
     for programacao in programacao_total["programme"]["entries"]:
-        if programacao["start_time"] == horario:
+        horario_inicial = programacao["start_time"]
+        horario_final = programacao["end_time"]
+        if horario_inicial <= horario <= horario_final:
             programacao_atual = {
                 "titulo" : programacao["title"],
                 "descricao": programacao["description"]
@@ -16,7 +22,7 @@ def execute_orchestrator_programming():
 
 
 def globo_get_endpoint():
-    data = "2020-07-14"
+    data = get_date()
     url = f"https://epg-api.video.globo.com/programmes/1337?date={data}"
     response = requests.get(url=url)
     data = json.loads(response.content)
@@ -27,4 +33,11 @@ def get_time_now():
     time_stamp = calendar.timegm(time.gmtime())
     return time_stamp
 
-execute_orchestrator_programming()
+def get_date():
+    tz_SP = pytz.timezone('America/Sao_Paulo')
+    time_sp = datetime.now(tz_SP)
+    time = time_sp.strftime("%Y-%m-%d")
+    return time
+
+
+# execute_orchestrator_programming()
